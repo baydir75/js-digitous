13a7018c872050f7524853e56c207f0a2cec8bbe
baydir <kidoni@live.fr>
2021-06-14 13:55:27 +0200

    Exo fini

---
+++ js/jour1.js
@@ -0,0 +1,56 @@
+console.log("Hello World");
+
+let test;
+
+test = "My Name is Baydir";
+
+console.log(test);
+
+let name;
+
+name = "Baydir";
+
+console.log("Nice to meet you " + name);
+
+let testLength;
+
+testLength = "I'm very long !";
+
+console.log(testLength.length);
+
+let food = "Croissant is meh";
+
+console.log(food.replace("meh", "good"));
+
+let basic = "This Is Cool";
+
+let basicUp;
+
+basicUp = basic.toUpperCase();
+
+let basicDown;
+
+basicDown = basic.toLowerCase();
+
+console.log(basicUp);
+
+let word;
+
+word = "banana";
+
+let letters = [];
+
+letters = word.split("");
+
+console.log(letters);
+
+let age;
+
+age = 27;
+
+let template;
+
+template = `I'm ${age} years old`;
+
+console.log(template);
+
