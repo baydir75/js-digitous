55aa507df0c5001881659897d3716d12ed95b0ff
baydir <kidoni@live.fr>
2021-06-16 16:51:14 +0200

    Manque bonus 4

--- js/jour3.js
+++ js/jour3.js
@@ -43,3 +43,41 @@ for (i = 0; i <= sentence.length; i++) {
 }
 console.log(sentence3.join(""));
 
+for (i = 0; i <= 100; i++) {
+    if (i % 3 == 0) {
+        console.log(`${i} : est un multiple de 3`);
+    } else if (i % 5 == 0) {
+        console.log(`${i} : est un multiple de 5`);
+        /* !!! */
+    } else if (i % 3 == 0 && i % 5 == 0) {
+        console.log(`${i} : est un multiple de 3 et 5`);
+    } else if (i % 7 == 0) {
+        console.log(`${i} : est un multiple de 7`);
+    }
+}
+
+total = 0;
+let count = 0;
+while (count <= limit) {
+    total = total + count;
+    count++;
+    console.log(total);
+}
+
+let promo = [];
+
+promo.push("Julien", "Anthony", "Elodie", "Eliott", "Laura", "Elizabeth", "Baydir", "Amadou");
+
+let tirageAuSort = Math.floor(Math.random() * 8);
+console.log(tirageAuSort);
+console.log(promo[tirageAuSort]);
+
+let integer = [];
+for (i = 0; i < 20; i++) {
+    integer.push(Math.round(Math.random() * (100 - 0) + 0));
+    console.log(integer);
+}
+
+for (i = 0; i < integer.length; i++) {
+    
+    }
